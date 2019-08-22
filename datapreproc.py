f91355e28520f378a074fbc004fc52d533935aa9
Shravan Chandrasekaran <shravan.chandrasekaran@gmail.com>
2019-08-22 13:19:06 -0400

    updating

--- datapreproc.py
+++ datapreproc.py
@@ -1,3 +1,9 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt 
 
 records = [json.loads(line) for line in open(url)]
 records_updated = [json.loads(json.dumps(records[0]['stationBeanList']))]
